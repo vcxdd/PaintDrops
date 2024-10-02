6b57ed0f5b59ceee67e9d90f9668817369d125ec
enymu <vinsonchiubusiness@gmail.com>
2024-10-02 10:03:06 -0400

    Add Vector and test cases

---
+++ ShapeLibrary/Vector.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary
+{
+    public struct Vector
+    {
+        public float X { get; }
+        public float Y { get; }
+
+        public Vector(float x, float y)
+        {
+            if (float.IsNaN(x) || float.IsNaN(y)) throw new ArgumentException("NaN values not allowed");
+
+            this.X = x;
+            this.Y = y;
+        }
+
+        public Vector(Vector v)
+        {
+            this.X = v.X;
+            this.Y = v.Y;
+        }
+
+        public static Double Magnitude(Vector v)
+        {
+            return Math.Sqrt((Math.Pow(v.X, 2) + Math.Pow(v.Y, 2)));
+        }
+
+        public static Vector Normalize(Vector v)
+        {
+            float length = (float)Magnitude(v);
+            float x = v.X / length;
+            float y = v.Y / length;
+
+            return new Vector(x, y);
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            float x = a.X + b.X;
+            float y = a.Y + b.Y;
+
+            return new Vector(x, y);
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            float x = a.X + -(b.X);
+            float y = a.Y + -(b.Y);
+
+            return new Vector(x, y);
+        }
+
+        public static Vector operator *(Vector v, float scalar)
+        {
+            if (float.IsNaN(scalar)) throw new ArgumentException("NaN values not allowed");
+
+            float x = v.X * scalar;
+            float y = v.Y * scalar;
+
+            return new Vector(x, y);
+        }
+
+        public static Vector operator /(Vector v, float scalar)
+        {
+            if (float.IsNaN(scalar)) throw new ArgumentException("NaN values not allowed");
+
+            float x = v.X / scalar;
+            float y = v.Y / scalar;
+
+            return new Vector(x, y);
+        }
+
+        public override String ToString()
+        {
+            return $"({this.X},{this.Y})";
+        }
+    }
+}
---
+++ ShapeLibraryTests/VectorTests.cs
@@ -0,0 +1,88 @@
+﻿using ShapeLibrary;
+
+namespace ShapeLibraryTests;
+
+[TestClass]
+public class VectorTests
+{
+    [TestMethod]
+    public void PlusOperatorTest()
+    {
+        Vector a = new Vector(0, 0);
+        Vector b = new Vector(1, 2);
+
+        Vector c = a + b;
+
+        Assert.AreEqual(1, c.X);
+        Assert.AreEqual(2, c.Y);
+    }
+
+    [TestMethod]
+    public void MinusOperatorTest()
+    {
+        Vector a = new Vector(0, 0);
+        Vector b = new Vector(1, 2);
+
+        Vector c = a - b;
+
+        Assert.AreEqual(-1, c.X);
+        Assert.AreEqual(-2, c.Y);
+    }
+
+    [TestMethod]
+    public void ScalarMultiplicationTest()
+    {
+        Vector v = new Vector(5, 7);
+        float scalar = 3;
+
+        Vector s = v * scalar;
+
+        Assert.AreEqual(15, s.X);
+        Assert.AreEqual(21, s.Y);
+    }
+
+    [TestMethod]
+    public void ScalarDivisionTest()
+    {
+        Vector v = new Vector(15, 7);
+        float scalar = 3;
+        float tolerance = 0.00001f;
+
+        Vector s = v / scalar;
+
+        Assert.AreEqual(5, s.X);
+        Assert.AreEqual(2.33333, s.Y, tolerance);
+    }
+
+    [TestMethod]
+    public void MagnitudeTest()
+    {
+        Vector v = new Vector(15, 7);
+        Double length = Vector.Magnitude(v);
+        float tolerance = 0.000001f;
+
+        Assert.AreEqual(16.552945, length, tolerance);
+    }
+
+    [TestMethod]
+    public void NormalizeTest()
+    {
+        Vector v = new Vector(15, 7);
+        Vector n = Vector.Normalize(v);
+        float tolerance = 0.000001f;
+
+        Assert.AreEqual(0.906183, n.X, tolerance);
+        Assert.AreEqual(0.422885, n.Y, tolerance);
+    }
+
+    [TestMethod]
+    public void ToStringTest()
+    {
+        Vector v = new Vector(15, 7);
+        Vector n = Vector.Normalize(v);
+        float tolerance = 0.000001f;
+
+        Assert.AreEqual(0.906183, n.X, tolerance);
+        Assert.AreEqual(0.422885, n.Y, tolerance);
+    }
+}
