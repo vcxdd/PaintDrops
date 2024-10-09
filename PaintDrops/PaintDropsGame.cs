742f6d484cbc0012d7a039ef8ba8c6db1cd8067d
enymu <vinsonchiubusiness@gmail.com>
2024-10-09 09:24:48 -0400

    Modify paintdrop game

---
+++ PaintDrops/PaintDropsGame.cs
@@ -0,0 +1,84 @@
+﻿using DrawingLibrary.Graphics;
+using DrawingLibrary.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using ShapeLibrary;
+
+namespace PaintDrops
+{
+    public class PaintDropsGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private IScreen screen;
+        private CustomKeyboard _keyboard;
+        private CustomMouse _mouse;
+        private Color _backgroundColor = Color.CornflowerBlue;
+        private ISpritesRenderer _spritesRenderer;
+        private List<ICircle> _circles = new List<ICircle>();
+
+        public PaintDropsGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = 640;
+            _graphics.PreferredBackBufferHeight = 480;
+            _graphics.ApplyChanges();
+
+            Window.AllowUserResizing = true;
+            Window.ClientSizeChanged += onClientSizeChanged;
+
+            this.screen = new Screen(new RenderTarget2D(GraphicsDevice, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height));
+            this._spritesRenderer = new SpritesRenderer(GraphicsDevice);
+
+            this._keyboard = CustomKeyboard.Instance;
+            this._mouse = CustomMouse.Instance;
+            _mouse.SetGraphicsDevice(GraphicsDevice);
+        }
+
+        private void onClientSizeChanged(Object sender, EventArgs e)
+        {
+            screen.CalculateDestinationRectangle();
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            _keyboard.Update();
+            _mouse.Update();
+            screen.CalculateDestinationRectangle();
+            if (_keyboard.IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
--- PaintDrops/Program.cs
+++ PaintDrops/Program.cs
@@ -1,3 +1,3 @@
 ﻿
-using var game = new PaintDrops.Game1();
+using var game = new PaintDrops.PaintDropsGame();
 game.Run();
