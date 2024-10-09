51127b1b73bdd823550a82d134e00813c1ccddce
enymu <vinsonchiubusiness@gmail.com>
2024-10-09 08:58:00 -0400

    Implement Circle

---
+++ ShapeLibrary/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary
+{
+    public class Circle : ICircle
+    {
+        public float Radius { get; }
+
+        public Vector Center { get;  }
+
+        public Vector[] Vertices { get; }
+
+        public Colour Colour { get; }
+
+        public Circle(float x, float y, float radius, Colour color)
+        {
+            this.Center = new Vector(x, y);
+            this.Radius = radius;
+            this.Colour = color;
+            this.Vertices = new Vector[100];
+            CalculateVertices(100);
+        }
+
+        private void CalculateVertices(float n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                float Xn = (float)(this.Center.X + this.Radius * Math.Cos(i * ((2 * Math.PI) / n)));
+                float Yn = (float)(this.Center.X + this.Radius * Math.Sin(i * ((2 * Math.PI) / n)));
+
+                Vertices[i] = new Vector(Xn, Yn);
+            }
+        }
+    }
+}
