e68745a19298259781c60d75f3c39fd45545c1c3
enymu <vinsonchiubusiness@gmail.com>
2024-10-21 09:11:43 -0400

    Add PaintDropSimulationFactory

---
+++ PaintDropSimulation/PaintDropSimulationFactory.cs
@@ -0,0 +1,23 @@
+﻿using ShapeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintDropSimulation
+{
+    public static class PaintDropSimulationFactory
+    {
+        public static IPaintDrop CreatePaintDrop(ICircle circle)
+        {
+            return new PaintDrop(circle);
+        }
+
+        public static ISurface CreateSurface(int width, int height)
+        {
+            return new Surface(width, height);
+        }
+    }
+}
