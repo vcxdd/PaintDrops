24b41cc0178018b707e859d51a9e7881289c769b
enymu <vinsonchiubusiness@gmail.com>
2024-11-20 10:00:46 -0500

    Create new console app

---
+++ PaintDropSimulationBenchmarking/Program.cs
@@ -0,0 +1,12 @@
+﻿using PaintDropSimulation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ISurface a = PaintDropSimulationFactory.CreateSurface(640, 480);
+        ISurface b = PaintDropSimulationFactory.CreateSurface(1000, 1000);
+        ISurface c = PaintDropSimulationFactory.CreateSurface(1280, 720);
+        ISurface d = PaintDropSimulationFactory.CreateSurface(1920, 1080);
+    }
+}
