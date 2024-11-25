5c4915c995d7e9256df0b0b4d853e045c794b2ac
enymu <vinsonchiubusiness@gmail.com>
2024-11-25 11:05:29 -0500

    Fix test due to perf optm

--- PatternGenerationLibTests/PatternGenerationLibTests.cs
+++ PatternGenerationLibTests/PatternGenerationLibTests.cs
@@ -41,5 +41,39 @@ namespace PaintDropTests
 
             Vector? v = p.CalculatePatternPoint(null);
         }
+
+        [TestMethod]
+        public void CalcTestSpiral()
+        {
+            var surfaceMock = new Mock<ISurface>();
+            surfaceMock.Setup(s => s.Width).Returns(1280);
+            surfaceMock.Setup(s => s.Height).Returns(720);
+            Spiral s = new Spiral();
+
+            Vector? v1 = s.CalculatePatternPoint(surfaceMock.Object);
+            Vector? v2 = s.CalculatePatternPoint(surfaceMock.Object);
+            Vector? v3 = s.CalculatePatternPoint(surfaceMock.Object);
+
+            Assert.IsNotNull(v1);
+            Assert.AreEqual(625.25, v1.Value.X, 0.1);
+            Assert.AreEqual(373.51, v1.Value.Y, 0.1);
+
+            Assert.IsNotNull(v2);
+            Assert.AreEqual(642.47, v2.Value.X, 0.1);
+            Assert.AreEqual(331.82, v2.Value.Y, 0.1);
+
+            Assert.IsNotNull(v3);
+            Assert.AreEqual(661.09, v3.Value.X, 0.1);
+            Assert.AreEqual(387.48, v3.Value.Y, 0.1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "surface must not be null")]
+        public void NullTest()
+        {
+            Phyllotaxis p = new Phyllotaxis();
+
+            Vector? v = p.CalculatePatternPoint(null);
+        }
     }
 }
--- ShapeLibraryTests/CircleTests.cs
+++ ShapeLibraryTests/CircleTests.cs
@@ -21,10 +21,10 @@ namespace ShapeLibraryTests
 
             Assert.AreEqual(108, c.Vertices[0].X);
             Assert.AreEqual(16, c.Vertices[0].Y);
-            Assert.AreEqual(107.998, c.Vertices[1].X, tolerance);
-            Assert.AreEqual(16.614, c.Vertices[1].Y, tolerance);
-            Assert.AreEqual(103.331, c.Vertices[50].X, tolerance);
-            Assert.AreEqual(46.201, c.Vertices[50].Y, tolerance);
+            Assert.AreEqual(107.970, c.Vertices[1].X, tolerance);
+            Assert.AreEqual(18.454, c.Vertices[1].Y, tolerance);
+            Assert.AreEqual(41.689, c.Vertices[50].X, tolerance);
+            Assert.AreEqual(110.154, c.Vertices[50].Y, tolerance);
 
         }
     }
